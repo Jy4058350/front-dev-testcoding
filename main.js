d82ceb762779ab7fed6ba4af0b0c287e6ba38d99
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2023-04-21 19:56:09 +0900

    class add

--- main.js
+++ main.js
@@ -1,18 +1,25 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const el = document.querySelector(".animate-title");
-  const el2 = document.querySelector(".animate-title2");
-  console.log(el);
-  const str = el.innerHTML.trim().split("");
-  const str2 = el2.innerHTML.trim().split("");
-  console.log(str);
-  let concatStr = "";
+  const ia = new TextAnimation(".bg-img");
+  const ta = new TextAnimation(".animate-title");
+  const ta2 = new TextAnimation(".animate-title2");
 
-  el.innerHTML = str.reduce((acc, curr) => {
-    curr = curr.replace(/\s+/, "&nbsp;");
-    return `${acc}<span class="char">${curr}</span>`;
-  }, "");
-  el2.innerHTML = str2.reduce((acc, curr) => {
-    curr = curr.replace(/\s+/, "&nbsp;");
-    return `${acc}<span class="char">${curr}</span>`;
-  }, "");
+  ta.animate();
+  ta2.animate();
 });
+
+class TextAnimation {
+  constructor(el) {
+    this.el = document.querySelector(el);
+    this.chars = this.el.innerHTML.trim().split("");
+    this.el.innerHTML = this._splitText();
+  }
+  _splitText() {
+    return this.chars.reduce((acc, curr) => {
+      curr = curr.replace(/\s+/, "&nbsp;");
+      return `${acc}<span class="char">${curr}</span>`;
+    }, "");
+  }
+  animate() {
+    this.el.classList.toggle("inview");
+  }
+}
