093fbd94a008b31e85d8eab63bc80f2b761e4d32
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2023-04-24 13:27:06 +0900

    gsap-splite add

---
+++ animation with splittext/main.js
@@ -0,0 +1,24 @@
+/*build*/
+
+const tl = gsap.timeline(),
+  mySplitText = new SplitText("#quote", { type: "words,chars" }),
+  chars = mySplitText.chars; //an array of all the divs that wrap each character
+
+
+console.log(chars);
+
+tl.from(chars, {
+  duration: 0.8,
+  opacity: 0,
+  scale: 0,
+  y: 10,
+  rotationX: 180,
+  transformOrigin: "0% 50% -50",
+  ease: "back",
+  stagger: 0.01
+});
+
+document.getElementById("animate").onclick = function () {
+  tl.restart();
+};
+
