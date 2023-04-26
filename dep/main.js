41ed192d71da9dea54c0c0dfd6fd22e212fe5b55
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2023-04-26 18:51:05 +0900

    strugle

--- dep/main.js
+++ dep/main.js
@@ -1,58 +1,40 @@
 document.addEventListener("DOMContentLoaded", function () {
-  
   const cb = function (el, isIntersecting) {
     if (isIntersecting) {
       // const ta = new TextAnimation(el);
       // ta.animate();
       // アニメーションの実行
       // .cover-slideのアニメーション
-      el.classList.add('inview');
-      
+      el.classList.add("inview");
     }
-    
   };
   const sot = new ScrollObserver(".animate-title", cb);
   const soi = new ScrollObserver(".pic", cb);
   const so = new ScrollObserver(".cover-slide", cb);
 
   const tl = gsap.timeline(),
-  mySplitText = new SplitText(".index", { type: "words,chars" }),
-  chars = mySplitText.chars; //an array of all the divs that wrap each character
+    mySplitText = new SplitText(".index", { type: "words,chars" }),
+    chars = mySplitText.chars; //an array of all the divs that wrap each character
 
-console.log(chars);
+  console.log(chars);
 
-tl.from(chars, {
-  duration: 0.8,
-  opacity: 0,
-  scale: 0,
-  y: 10,
-  rotationX: 180,
-  transformOrigin: "0% 50% -50",
-  ease: "back",
-  stagger: 0.01,
+  tl.from(chars, {
+    duration: 0.8,
+    opacity: 0,
+    scale: 0,
+    y: 10,
+    rotationX: 180,
+    transformOrigin: "0% 50% -50",
+    ease: "back",
+    stagger: 0.01,
+  });
 });
 
+//swiper js//
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  // direction: 'vertical',
+  loop: true,
+  effect: "coverflow",
+  centeredSlides: true,
 });
-
-/*表紙のアニメーション*/
-
-// const tl = gsap.timeline(),
-//   mySplitText = new SplitText(".index", { type: "words,chars" }),
-//   chars = mySplitText.chars; //an array of all the divs that wrap each character
-
-// console.log(chars);
-
-// tl.from(chars, {
-//   duration: 0.8,
-//   opacity: 0,
-//   scale: 0,
-//   y: 10,
-//   rotationX: 180,
-//   transformOrigin: "0% 50% -50",
-//   ease: "back",
-//   stagger: 0.01,
-// });
-
-// document.getElementById("animate").onclick = function () {
-//   tl.restart();
-// };
