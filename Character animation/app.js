2481bf51b7db50419622d464bdae146064e21db9
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2023-05-02 15:07:27 +0900

    CreateingClassScrollMonitoring comp

--- Character animation/app.js
+++ Character animation/app.js
@@ -1,30 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const els = document.querySelectorAll(".animation-title");
-  els.forEach((el) => {
-    io.observe(el);
-  });
-});
-
-const cb = function (entries, observer) {
-  //IntersectionObserverは、コールバック関数に対して、
-  //IntersectionObserverEntryの配列を引数として渡します。
-  //その配列の中には、監視対象の要素が含まれています。
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      //監視対象の要素が画面に入ったときの処理
-      const ta = new TextAnimation(entry.target);
+  const cb = function (el, isIntersecting) {
+    if (isIntersecting) {
+      const ta = new TextAnimation(el);
       ta.animate();
-      observer.unobserve(entry.target);
-    } else {
-      //監視対象の要素が画面から出たときの処理
     }
-  });
-};
-
-const options = {
-  root: null,
-  rootMargin: "-100px 0px",
-  threshold: 0,
-};
-
-const io = new IntersectionObserver(cb, options);
+  };
+  const so = new ScrollObserver(".animation-title", cb, { once: true });
+});
